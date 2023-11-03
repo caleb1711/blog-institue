820ec8a9e0f9f16e31d4909592e17e557843661a
caleb1711 <calkra15@outlook.com>
2023-11-03 23:29:57 +0500

    core

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 
+# Email smtp setting
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
