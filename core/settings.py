26fc1200b67b196ae62b187f2cb828e87c7e08a0
caleb1711 <calkra15@outlook.com>
2023-11-07 01:59:29 +0500

    update procfile and settings

--- core/settings.py
+++ core/settings.py
@@ -114,9 +114,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 # FOR LIVE SERVER 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
