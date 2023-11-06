09d93144ef8ddd43e5844f889621ebdab8c5a103
caleb1711 <calkra15@outlook.com>
2023-11-07 02:20:34 +0500

    fix bug

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
