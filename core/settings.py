e6f859f0a97930976fcd058ce485c7f659586f22
caleb1711 <calkra15@outlook.com>
2023-11-07 01:57:38 +0500

    update static files settings

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 # FOR LIVE SERVER 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # media 
