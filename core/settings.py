c7166415680de34241f7fbc56b61bd7e6d3f59f4
caleb1711 <calkra15@outlook.com>
2023-11-07 01:48:22 +0500

    update settings

--- core/settings.py
+++ core/settings.py
@@ -14,12 +14,12 @@ env = environ.Env(
 )
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=[])
 
